87c75b1685e71afac1b18f9af24734b2f54c90c6
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-11-03 20:15:01 +0300

    third commit

--- app/src/main/java/com/bwisa/blogapp/AuthActivity.java
+++ app/src/main/java/com/bwisa/blogapp/AuthActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.bwisa.blogapp.Fragments.SignInFragment;
+
 public class AuthActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
+
+        //5.1 make sign in fragment as initial screen of AuthActivity
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer,new SignInFragment()).commit();
     }
 }
