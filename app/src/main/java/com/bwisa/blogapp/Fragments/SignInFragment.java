5e67975facd8b7f2ca3e4c0b916969cfd46f4906
lynseybwisa <lynsey.bwisa@strathmore.edu>
2020-11-10 19:27:45 +0300

    fourth commit

--- app/src/main/java/com/bwisa/blogapp/Fragments/SignInFragment.java
+++ app/src/main/java/com/bwisa/blogapp/Fragments/SignInFragment.java
@@ -1,5 +1,7 @@
 package com.bwisa.blogapp.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,14 +10,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bwisa.blogapp.Constant;
 import com.bwisa.blogapp.R;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //4.1 make fragments and their layouts
 public class SignInFragment extends Fragment {
@@ -24,6 +39,7 @@ public class SignInFragment extends Fragment {
     private TextInputEditText txtEmail, txtPassword;
     private TextView txtSignUp;
     private Button btnSignIn;
+    private ProgressDialog dialog;
 
     public SignInFragment(){}
 
@@ -43,6 +59,8 @@ public class SignInFragment extends Fragment {
         txtSignUp = view.findViewById(R.id.txtSignUp);
         txtEmail = view.findViewById(R.id.txtEmailSignIn);
         btnSignIn= view.findViewById(R.id.btnSignIn);
+        dialog = new ProgressDialog(getContext());
+        dialog.setCancelable(false);
 
         txtSignUp.setOnClickListener(v -> {
             //4.3 change fragments
@@ -52,7 +70,7 @@ public class SignInFragment extends Fragment {
         //4.4 validate the fields first
         btnSignIn.setOnClickListener(v -> {
             if (validate()){
-
+               login();
             }
         });
 
@@ -97,6 +115,7 @@ public class SignInFragment extends Fragment {
         });
     }
 
+
     //4.5 add errors
     private boolean validate(){
         if (txtEmail.getText().toString().isEmpty()){
@@ -110,5 +129,51 @@ public class SignInFragment extends Fragment {
             return false;
         }
         return true;
+    }
+
+    //4.6
+    private void login() {
+        dialog.setMessage("Logging in");
+        dialog.show();
+        StringRequest request = new StringRequest(Request.Method.POST, Constant.LOGIN,  response ->{
+            //we get response if connection is successful
+            try {
+                JSONObject object = new JSONObject(response);
+                if (object.getBoolean("success")){
+                    JSONObject user = object.getJSONObject("user");
+                    //make shared preference user
+                    SharedPreferences userPref = getActivity().getApplicationContext().getSharedPreferences("user", getContext().MODE_PRIVATE);
+                    SharedPreferences.Editor editor = userPref.edit();
+                    editor.putString("token", object.getString("token"));
+                    editor.putString("name", user.getString("name"));
+                    editor.putString("lastname", user.getString("lastname"));
+                    editor.putString("photo", user.getString("photo"));
+                    editor.apply();
+                    //if successful
+                    Toast.makeText(getContext(),"Login success", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            dialog.dismiss();
+        }, error -> {
+            //error displayed
+            error.printStackTrace();
+            dialog.dismiss();
+        }){
+            //add parameters
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("email", txtEmail.getText().toString().trim());
+                map.put("password", txtPassword.getText().toString());
+                return map;
+            }
+        };
+
+        // add this request to request queue
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(request);
     }
 }
--- app/src/main/java/com/bwisa/blogapp/Fragments/SignUpFragment.java
+++ app/src/main/java/com/bwisa/blogapp/Fragments/SignUpFragment.java
@@ -1,5 +1,7 @@
 package com.bwisa.blogapp.Fragments;
 
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -8,14 +10,27 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bwisa.blogapp.Constant;
 import com.bwisa.blogapp.R;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 //5.1 make fragments and their layouts
 public class SignUpFragment extends Fragment {
@@ -25,6 +40,7 @@ public class SignUpFragment extends Fragment {
     private TextInputEditText txtEmail, txtPassword, txtConfirm;
     private TextView txtSignIn;
     private Button btnSignUp;
+    private ProgressDialog dialog;
 
     public SignUpFragment(){}
 
@@ -45,6 +61,8 @@ public class SignUpFragment extends Fragment {
         txtSignIn = view.findViewById(R.id.txtSignIn);
         txtEmail = view.findViewById(R.id.txtEmailSignUp);
         btnSignUp= view.findViewById(R.id.btnSignUp);
+        dialog = new ProgressDialog(getContext());
+        dialog.setCancelable(false);
 
         txtSignIn.setOnClickListener(v -> {
             //4.3 change fragments
@@ -54,7 +72,7 @@ public class SignUpFragment extends Fragment {
         //4.4 validate the fields first
         btnSignUp.setOnClickListener(v -> {
             if (validate()){
-
+                register();
             }
         });
 
@@ -118,6 +136,7 @@ public class SignUpFragment extends Fragment {
         });
     }
 
+
     //4.5 add errors
     private boolean validate(){
         if (txtEmail.getText().toString().isEmpty()){
@@ -137,4 +156,51 @@ public class SignUpFragment extends Fragment {
         }
         return true;
     }
-}
+
+    //4.6
+    private void register() {
+        dialog.setMessage("Registering");
+        dialog.show();
+        StringRequest request = new StringRequest(Request.Method.POST, Constant.REGISTER, response ->{
+            //we get response if connection is successful
+            try {
+                JSONObject object = new JSONObject(response);
+                if (object.getBoolean("success")){
+                    JSONObject user = object.getJSONObject("user");
+                    //make shared preference user
+                    SharedPreferences userPref = getActivity().getApplicationContext().getSharedPreferences("user", getContext().MODE_PRIVATE);
+                    SharedPreferences.Editor editor = userPref.edit();
+                    editor.putString("token", object.getString("token"));
+                    editor.putString("name", user.getString("name"));
+                    editor.putString("lastname", user.getString("lastname"));
+                    editor.putString("photo", user.getString("photo"));
+                    editor.apply();
+                    //if successful
+                    Toast.makeText(getContext(),"Register success", Toast.LENGTH_SHORT).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            dialog.dismiss();
+        }, error -> {
+            //error displayed
+            error.printStackTrace();
+            dialog.dismiss();
+        }){
+            //add parameters
+
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String,String> map = new HashMap<>();
+                map.put("email", txtEmail.getText().toString().trim());
+                map.put("password", txtPassword.getText().toString());
+                return map;
+            }
+        };
+
+        // add this request to request queue
+        RequestQueue queue = Volley.newRequestQueue(getContext());
+        queue.add(request);
+    }
+    }
+
